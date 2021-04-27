31e4517f907d5c4f8f69a223e0e68d4283a5db23
Matej <pesjak.matej@gmail.com>
2021-04-27 21:03:49 +0200

    Added landing page beautiful design

--- client/src/components/LandingPage/LandingPage.tsx
+++ client/src/components/LandingPage/LandingPage.tsx
@@ -2,14 +2,19 @@ import './LandingPage.css';
 import { useHistory } from "react-router-dom";
 import { demoLogin } from '../../controllers/UserController';
 import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+const AOS = require('aos');
 
 function LandingPage() {
     const history = useHistory();
     const dispatch = useDispatch();
-
     const navigate = (destination: string) => {
         history.push(destination);
     }
+
+    useEffect(() => {
+        AOS.init({ duration: 1500 });
+    }, []);
     return (
         <div className="landingPage">
             <section id="section1" className="firstPage">
@@ -19,7 +24,11 @@ function LandingPage() {
                         <div className="signUp" onClick={() => navigate('/signup')}>Sign up</div>
                     </div>
                     <div className="firstPageContentContainer">
-                        <h1 className="title"> Let's track progress together</h1>
+                        <h1 className="title typing1"> Let's track</h1><br></br>
+                        <h1 className="title typing2">
+                            progress</h1><br></br>
+                        <h1 className="title typing3">
+                            together</h1>
                         <h3 className="subTitle" >Track progress and watch how you progress in your fitness journey</h3>
                         <div className="signUpAndLearnMore">
                             {/*<div className="signUpNow" onClick={() => navigate('/signup')}>Sign up now</div>
